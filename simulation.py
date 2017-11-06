c391c6c681ff6b921a3053ce5ffd5634b88a147a
Ethan Peterson <cap2010.ep@gmail.com>
2017-11-06 16:09:18 -0700

    Initial commit. Modify NetworkPacket to have a src.

--- simulation.py
+++ simulation.py
@@ -7,57 +7,66 @@ import network
 import link
 import threading
 from time import sleep
+from collections import defaultdict
 
 ##configuration parameters
 router_queue_size = 0 #0 means unlimited
 simulation_time = 1 #give the network sufficient time to transfer all packets before quitting
 
+# creates a routing table from a sequence of src/dst pairings
+def createRoutingTable(seq):
+    d = defaultdict(int)
+    for k, v in seq:
+        d[k].append(v)
+
+    return d
+
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
-    
+
     #create network nodes
     client = network.Host(1)
     object_L.append(client)
     server = network.Host(2)
     object_L.append(server)
-    router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size, routing_table=)
     object_L.append(router_a)
-    
+
     #create a Link Layer to keep track of links between network nodes
     link_layer = link.LinkLayer()
     object_L.append(link_layer)
-    
+
     #add all the links
     link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
     link_layer.add_link(link.Link(router_a, 0, server, 0, 50))
-    
-    
+
+
     #start all the objects
     thread_L = []
     thread_L.append(threading.Thread(name=client.__str__(), target=client.run))
     thread_L.append(threading.Thread(name=server.__str__(), target=server.run))
     thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
-    
+
     thread_L.append(threading.Thread(name="Network", target=link_layer.run))
-    
+
     for t in thread_L:
         t.start()
-    
-    
-    #create some send events    
+
+
+    #create some send events
     for i in range(3):
         client.udt_send(2, 'Sending a much larger message this time for the first part of the thing %d' % i)
-    
-    
+
+
     #give the network sufficient time to transfer all packets before quitting
     sleep(simulation_time)
-    
+
     #join all threads
     for o in object_L:
         o.stop = True
     for t in thread_L:
         t.join()
-        
+
     print("All simulation threads joined")
 
 
